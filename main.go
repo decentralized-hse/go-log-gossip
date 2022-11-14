39f84c4cbe8e8802c379ecf8db6ca84a7357b31a
denisSurkov <surkov0denis@yandex.ru>
2022-11-14 15:02:21 +0500

    Simple server to add new log

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"context"
+	"github.com/decentralized-hse/go-log-gossip/gui"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
 func main() {
+	log.Printf("main: starting HTTP server")
+
+	server := gui.StartGUIServer()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+	}
 }
